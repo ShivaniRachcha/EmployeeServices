4b6a83d4767cfceae9c1ce6cd3f5643f04af570e
ShivaniRachcha <129090187+ShivaniRachcha@users.noreply.github.com>
2023-03-28 14:10:30 +0530

    Update EmployeeServicesApplication.java

--- src/main/java/demo/EmployeeServices/EmployeeServicesApplication.java
+++ src/main/java/demo/EmployeeServices/EmployeeServicesApplication.java
@@ -2,12 +2,18 @@ package demo.EmployeeServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeServicesApplication {
+public class EmployeeServicesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServicesApplication.class, args);
 	}
 
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(EmployeeServicesApplication.class);
+	  }
 }
